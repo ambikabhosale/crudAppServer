bf123c98905c666c732c445cff1405a5caa03012
AmbikaBhosale <bhosaleambika123@gmail.com>
2020-10-23 10:35:51 +0530

    get, post, delete, post

--- server.js
+++ server.js
@@ -15,14 +15,17 @@ crudApp.use(bodyParser.json());
 crudApp.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
 
 MongoClient.connect(connectionString, { useUnifiedTopology: true }, function(err, db) {
   if (err) throw err;
-  console.log("Connectd");
+  console.log("Connected");
   var db = db.db("test_db");
   const quotesCollection = db.collection('india-cities')
+  
   //CREATE OPERATION
   crudApp.post('/cities', (req, res) => {
     quotesCollection.insertOne(req.body) //insertOne is the method to add data to collection
@@ -31,14 +34,36 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true }, function(err
      })
      .catch(error => console.error(error))
  });
+
  //READ OPERATION
  crudApp.get('/cities',(req,res)=>{
     const cursor = db.collection('india-cities').find().toArray()
     .then(results => {
-     res.send(results);
+      res.send(results);
     })
     .catch(error => console.error(error))
  });
+
+  //Delete OPERATION
+  crudApp.delete('/cities/:id',(req,res)=>{
+    if (err) throw err;
+    var myquery = { _id: req.params.id };
+    quotesCollection.deleteOne(myquery, function(err, obj) {
+      if (err) throw err;
+      res.send(obj);
+    });
+  });
+
+  //update OPERATION
+  crudApp.put('/cities/:id',(req,res)=>{
+    console.log(req);
+    if (err) throw err;
+    var myquery = { _id: req.params.id };
+    quotesCollection.updateOne(myquery, {$set: req.body}, { upsert: true }, function(err, obj) {
+      if (err) throw err;
+      res.send(obj);
+    });
+  });
 
 });
 
